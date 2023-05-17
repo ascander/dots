bc3543d3c8efaeb1cd00697244c482a6fe2c0e9c
Ascander Dost <1815984+ascander@users.noreply.github.com>
2023-05-17 14:07:22 -0700

    Updates nixpkgs URL

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Ascander's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin.url = "github:lnl7/nix-darwin/master";
