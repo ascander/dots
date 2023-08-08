f8127d2903abe486cd0340f906ba292db3ae5050
Ascander Dost <1815984+ascander@users.noreply.github.com>
2023-08-08 14:51:28 -0700

    Adds IBM Plex font and switches to it

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -75,7 +75,7 @@
   # NOTE: this removes any manually added fonts
   fonts.fontDir.enable = true;
   fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraMono" "VictorMono" ]; })
+    (nerdfonts.override { fonts = [ "FiraMono" "IBMPlexMono" ]; })
     source-sans-pro
   ];
 
