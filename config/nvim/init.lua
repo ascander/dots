ef17408a0f7db8f4f8038fab5c1724c3903a8051
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-15 16:50:16 -0700

    Adds lua config and plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,31 +1,15 @@
---[[
+-- Enable the experimental Lua module loader
+-- See `:help loader`
+vim.loader.enable()
 
-Neovim init file
+-- Disable netrw
+-- See `:help netrw-noload`
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-Not that we needed all that for the trip, but once you get locked into a
-serious drug collection, the tendency is to push it as far as you can.
-
-                                                           - Hunter S. Thompson
-
---]]
-
--- Import Lua modules
-for _, source in ipairs({
-  "core.options",
-  "core.autocmds",
-  "core.colors",
-  "core.keymaps",
-  "core.statusline",
-  "lsp.diagnostics",
-  "lsp.lspconfig",
-  "lsp.metals",
-  "plugins.telescope",
-  "plugins.cmp",
-  "plugins.treesitter",
-  "plugins.comment",
-}) do
-  local ok, fault = pcall(require, source)
-  if not ok then
-    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
-  end
-end
+-- Load modules
+require("user.options")
+require("user.keymaps")
+require("user.colorscheme")
+require("user.nvim-tree")
+-- require("user.comment")
