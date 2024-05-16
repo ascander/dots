937a0a834815412514ad0b41e6384e22e71d1599
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-16 12:45:55 -0700

    Adds telescope.nvim

---
+++ config/nvim/lua/user/telescope.lua
@@ -0,0 +1,40 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
+
+telescope.setup({
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    color_devicons = true,
+    set_env = { ["COLORTERM"] = "truecolor" },
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.cycle_history_next,
+	["<C-p>"] = actions.cycle_history_prev,
+
+	["<C-j>"] = actions.move_selection_next,
+	["<C-k>"] = actions.move_selection_previous,
+      },
+
+      n = {
+	["q"] = actions.close,
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+	override_generic_sorter = true,
+	override_file_sorter = true,
+	case_mode = "smart_case",
+      },
+    },
+  },
+})
+
+telescope.load_extension('fzf')
