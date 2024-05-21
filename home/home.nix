29acfceb39b1f33bf855d891a77c260fde8e753a
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-21 10:30:18 -0700

    Removes tmux continuum

--- home/home.nix
+++ home/home.nix
@@ -118,13 +118,14 @@ in {
         plugin = resurrect;
         extraConfig = "set -g @resurrect-strategy-nvim 'session'";
       }
-      {
-        plugin = continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '60' # minutes
-        '';
-      }
+      # FIXME: troubleshoot weirdness around duplicate sessions
+      # {
+      #   plugin = continuum;
+      #   extraConfig = ''
+      #     set -g @continuum-restore 'on'
+      #     set -g @continuum-save-interval '60' # minutes
+      #   '';
+      # }
     ];
     extraConfig = builtins.readFile ../config/tmux/tmux.conf;
   };
