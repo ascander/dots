f5eca7830d8d980e37318daf211af0c9640fc792
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-21 12:29:16 -0700

    Plumbs neovim nightly overlay

--- flake.nix
+++ flake.nix
@@ -16,24 +16,60 @@
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs =
-    { self
-    , nixpkgs
-    , darwin
-    , home-manager
-    , ...
-    } @ inputs:
-    let
-      inherit (darwin.lib) darwinSystem;
-      inherit (inputs.nixpkgs.lib) attrValues optionalAttrs singleton;
+  outputs = {
+    self,
+    nixpkgs,
+    darwin,
+    home-manager,
+    neovim-nightly-overlay,
+    ...
+  } @ inputs: let
+    inherit (darwin.lib) darwinSystem;
+    inherit (inputs.nixpkgs.lib) attrValues optionalAttrs singleton;
 
       currentSystem = "x86_64-darwin";
 
-      # Configuration for 'nixpkgs'
-      nixpkgsConfig = {
-        config = { allowUnfree = true; };
-        overlays =
-          attrValues self.overlays
+    # Configuration for 'nixpkgs'
+    nixpkgsConfig = {
+      config = {allowUnfree = true;};
+      overlays = singleton neovim-nightly-overlay.overlay;
+    };
+  in {
+    overlays = {
+      # # Adds access to (unstable) x86 packages through 'pkgs.x86' if running Apple Silicon
+      # apple-silicon = _: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+      #   x86 = import inputs.nixpkgs-unstable {
+      #     system = "x86_64-darwin";
+      #     inherit (nixpkgsConfig) config;
+      #   };
+      # };
+
+      # # Substitute x86 versions of packages that don't build on Apple Silicon yet
+      # sub-x86 = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+      #   inherit (final.x86);
+      # };
+
+      # Adds access to unstable packages through 'pkgs.unstable'
+      pkgs-unstable = _: prev: {
+        unstable = import inputs.nixpkgs-unstable {
+          inherit (prev.stdenv) system;
+          inherit (nixpkgsConfig) config;
+        };
+      };
+    };
+
+    darwinModules = {
+      bootstrap = import ./darwin/bootstrap.nix;
+      defaults = import ./darwin/defaults.nix;
+      configuration = import ./darwin/configuration.nix;
+      homebrew = import ./darwin/homebrew.nix;
+    };
+
+    darwinConfigurations = {
+      adost-ltm = darwinSystem {
+        system = "x86_64-darwin";
+        modules =
+          attrValues self.darwinModules
           ++ [
             inputs.neovim-nightly-overlay.overlay
           ];
