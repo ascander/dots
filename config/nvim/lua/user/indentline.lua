fdbf85ccfc3020c84e62397208ba9b586d6369b7
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-23 14:32:47 -0700

    Formats Lua sources

--- config/nvim/lua/user/indentline.lua
+++ config/nvim/lua/user/indentline.lua
@@ -1,34 +1,34 @@
 local M = {
-  "lukas-reineke/indent-blankline.nvim",
-  event = "VeryLazy",
-  commit = "9637670896b68805430e2f72cf5d16be5b97a22a",
-  main = "ibl",
+	"lukas-reineke/indent-blankline.nvim",
+	event = "VeryLazy",
+	commit = "9637670896b68805430e2f72cf5d16be5b97a22a",
+	main = "ibl",
 }
 
 function M.config()
-  local icons = require "user.icons"
+	local icons = require("user.icons")
 
-  require("ibl").setup {
-    indent = { char = icons.ui.LineMiddle },
-    whitespace = {
-      remove_blankline_trail = true,
-    },
+	require("ibl").setup({
+		indent = { char = icons.ui.LineMiddle },
+		whitespace = {
+			remove_blankline_trail = true,
+		},
 
-    exclude = {
-      filetypes = {
-        "help",
-        "startify",
-        "dashboard",
-        "lazy",
-        "neogitstatus",
-        "NvimTree",
-        "Trouble",
-        "text",
-      },
-      buftypes = { "terminal", "nofile" },
-    },
-    scope = { enabled = false },
-  }
+		exclude = {
+			filetypes = {
+				"help",
+				"startify",
+				"dashboard",
+				"lazy",
+				"neogitstatus",
+				"NvimTree",
+				"Trouble",
+				"text",
+			},
+			buftypes = { "terminal", "nofile" },
+		},
+		scope = { enabled = false },
+	})
 end
 
 return M
--- config/nvim/lua/user/mason.lua
+++ config/nvim/lua/user/mason.lua
@@ -1,28 +1,27 @@
 local M = {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-  },
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
 }
 
-
 function M.config()
-  local servers = {
-    "lua_ls",
-    "pyright",
-    "bashls",
-    "jsonls",
-  }
+	local servers = {
+		"lua_ls",
+		"pyright",
+		"bashls",
+		"jsonls",
+	}
 
-  require("mason").setup {
-    ui = {
-      border = "rounded",
-    },
-  }
+	require("mason").setup({
+		ui = {
+			border = "rounded",
+		},
+	})
 
-  require("mason-lspconfig").setup {
-    ensure_installed = servers,
-  }
+	require("mason-lspconfig").setup({
+		ensure_installed = servers,
+	})
 end
 
 return M
