566bb603c9597c4733e2cb250b5845243f60e691
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-23 09:15:09 -0700

    Adds config for nvim-tree

--- config/nvim/lua/user/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-local plugin = "nvim-tree"
-local status_ok, nvimtree = pcall(require, plugin)
-if not status_ok then
-  vim.notify("plugin " .. plugin .. " not found!")
-  return
-end
-
-nvimtree.setup({})
