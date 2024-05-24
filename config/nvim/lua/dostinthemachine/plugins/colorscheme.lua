500289a168578860d8a4ca0234c5eed3839a9345
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-24 12:03:42 -0700

    Adds tmux-navigation

--- config/nvim/lua/dostinthemachine/plugins/colorscheme.lua
+++ config/nvim/lua/dostinthemachine/plugins/colorscheme.lua
@@ -1,7 +1,20 @@
 return {
-  { "folke/tokyonight.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
-  { "rebelot/kanagawa.nvim" },
+  -- tokyonight.nvim
+  -- https://github.com/folke/tokyonight.nvim
+  { "folke/tokyonight.nvim", lazy = true },
+
+  -- rose-pine/neovim
+  -- https://github.com/rose-pine/neovim
+  { "rose-pine/neovim", name = "rose-pine", lazy = true },
+
+  -- kanagawa.nvim
+  -- https://github.com/rebelot/kanagawa.nvim
+  { "rebelot/kanagawa.nvim", lazy = true },
+
+  -- nightfox.nvim
+  -- https://github.com/EdenEast/nightfox.nvim
+  --
+  -- See https://github.com/folke/lazy.nvim?tab=readme-ov-file#examples
   {
     "EdenEast/nightfox.nvim",
     lazy = false,
