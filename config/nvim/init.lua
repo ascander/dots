5cb786150d7e0ace8ab722ff7909bfeb40b73941
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-24 10:35:03 -0700

    Restructures skeleton

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,33 +1,9 @@
-require("user.launch")
-require("user.options")
-require("user.keymaps")
-require("user.autocmds")
+-- Set leader key before plugins are loaded
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-spec("user.tmuxnavigation")
-spec("user.colorscheme")
-spec("user.devicons")
-spec("user.treesitter")
-spec("user.telescope")
-spec("user.mason")
-spec("user.schemastore")
-spec("user.lspconfig")
-spec("user.cmp")
-spec("user.none-ls")
-spec("user.illuminate")
-spec("user.gitsigns")
-spec("user.whichkey")
-spec("user.nvimtree")
-spec("user.comment")
-spec("user.lualine")
-spec("user.navic")
-spec("user.breadcrumbs")
-spec("user.harpoon")
-spec("user.neotest")
-spec("user.autopairs")
-spec("user.neogit")
-spec("user.dashboard")
-spec("user.project")
-spec("user.indentline")
-spec("user.toggleterm")
-
-require("user.lazy")
+-- Set core options
+require "dostinthemachine.options"
+require "dostinthemachine.keymaps"
+require "dostinthemachine.autocmds"
+require "dostinthemachine.lazy"
