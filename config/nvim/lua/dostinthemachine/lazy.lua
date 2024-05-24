32e0c1eb114b0f481baa6b2e9a402a7a3825e8ab
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-24 10:55:01 -0700

    Adds first plugin spec

--- config/nvim/lua/dostinthemachine/lazy.lua
+++ config/nvim/lua/dostinthemachine/lazy.lua
@@ -1,2 +1,23 @@
--- Bootstrap lazy.nvim here
--- Import plugin specs here
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme terafox")
+    end
+  }
+})
