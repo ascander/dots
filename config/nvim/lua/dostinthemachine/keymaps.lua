21a96fdb85345cc0b6073f51b703f8766d394ac6
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-24 10:45:05 -0700

    Adds keymaps.lua

--- config/nvim/lua/dostinthemachine/keymaps.lua
+++ config/nvim/lua/dostinthemachine/keymaps.lua
@@ -1,1 +1,123 @@
--- General (core) keymaps go here
+-- Sets a keymap using Neovim's API
+---@param mode
+---@param lhs
+---@param rhs
+---@param opts
+local map = function(mode, lhs, rhs, opts)
+  local base_opts = { noremap = true, silent = true }
+  local keymap_opts = vim.tbl_deep_extend("force", base_opts, opts or {})
+
+  vim.keymap.set(mode, lhs, rhs, keymap_opts)
+end
+
+-- Clear highlight on pressing <Esc> in normal mode
+map({ "i", "n" }, "<Esc>", "<cmd>nohlsearch<CR><Esc>", opts)
+
+-- Better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase Window Width" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<CR>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>m .-2<CR>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<CR>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<CR>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move Up" })
+
+-- Buffers
+map("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Prev Buffer" })
+map("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
+map("n", "[b", "<cmd>bprevious<CR>", { desc = "Prev Buffer" })
+map("n", "]b", "<cmd>bnext<CR>", { desc = "Next Buffer" })
+map("n", "<leader>bb", "<cmd>e #<CR>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<CR>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+map(
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+-- Add undo break-points
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", ";", ";<c-g>u")
+
+-- Save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<CR><Esc>", { desc = "Save File" })
+
+-- keywordprg
+map("n", "<leader>K", "<cmd>norm! K<CR>", { desc = "Keywordprg" })
+
+-- Better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- New file
+map("n", "<leader>fn", "<cmd>enew<CR>", { desc = "New File" })
+
+-- Quickfix lists
+map("n", "<leader>xl", "<cmd>lopen<CR>", { desc = "Location List" })
+map("n", "<leader>xq", "<cmd>copen<CR>", { desc = "Quickfix List" })
+map("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
+
+-- Diagnostics
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go { severity = severity }
+  end
+end
+
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- Quit
+map("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit All" })
+
+-- Highlights under cursor
+map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+
+-- Windows
+map("n", "<leader>ww", "<C-W>p", { desc = "Other Window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
+map("n", "<leader>w-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+
+-- Tabs
+map("n", "<leader><tab>l", "<cmd>tablast<CR>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<CR>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<CR>", { desc = "New Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<CR>", { desc = "Next Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<CR>", { desc = "Close Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<CR>", { desc = "Previous Tab" })
