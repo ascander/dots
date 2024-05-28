ae2e5e8dfb0847878031255be1acc8dc39fc7833
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-28 16:33:51 -0700

    Adds aerial.nvim

--- config/nvim/lua/dostinthemachine/plugins/lspconfig.lua
+++ config/nvim/lua/dostinthemachine/plugins/lspconfig.lua
@@ -209,5 +209,40 @@ return {
         ensure_installed()
       end
     end,
+  },
+
+  {
+    "stevearc/aerial.nvim",
+    cmd = "AerialToggle",
+    opts = function()
+      local icons = require("dostinthemachine.icons").kind
+
+      local opts = {
+        attach_mode = "global",
+        backends = { "lsp", "treesitter", "markdown", "man" },
+        show_guides = true,
+        layout = {
+          resize_to_content = false,
+          win_opts = {
+            winhl = "Normal:NormalFloat,FloatBorder:NormalFloat,SignColumn:SignColumnSB",
+            signcolumn = "yes",
+            statuscolumn = " ",
+          },
+        },
+        icons = icons,
+        -- stylua: ignore
+        guides = {
+          mid_item   = "├╴",
+          last_item  = "└╴",
+          nested_top = "│ ",
+          whitespace = "  ",
+        },
+      }
+
+      return opts
+    end,
+    keys = {
+      { "<leader>cs", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
+    },
   }
 }
