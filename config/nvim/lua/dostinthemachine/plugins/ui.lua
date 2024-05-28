9a755ef3006d2d7672f5bce6b00fe1e1a5c4aaa9
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-28 16:33:41 -0700

    Adds lualine.nvim

--- config/nvim/lua/dostinthemachine/plugins/ui.lua
+++ config/nvim/lua/dostinthemachine/plugins/ui.lua
@@ -1,4 +1,89 @@
 return {
+
+  -- lualine.nvim
+  -- https://github.com/nvim-lualine/lualine.nvim
+  -- Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" }
+    },
+    init = function()
+      vim.g.lualine_laststatus = vim.o.laststatus
+      if vim.fn.argc(-1) > 0 then
+        -- set an empty statusline till lualine loads
+        vim.o.statusline = " "
+      else
+        -- hide the statusline on the starter page
+        vim.o.laststatus = 0
+      end
+    end,
+    opts = function()
+      local lualine_require = require("lualine_require")
+      lualine_require.require = require
+
+      local icons = require("dostinthemachine.icons")
+
+      vim.o.laststatus = vim.g.lualine_laststatus
+
+      return {
+        options = {
+          theme = "auto",
+          globalstatus = true,
+          disabled_filetypes = { statusline = { "dashboard" } },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+
+          lualine_c = {
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          },
+          lualine_x = {
+            {
+              "diff",
+              symbols = {
+                added = icons.git.added,
+                modified = icons.git.modified,
+                removed = icons.git.removed,
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
+          },
+          lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+          },
+          lualine_z = {
+            function()
+              return icons.misc.Watch .. " " .. os.date("%R")
+            end,
+          },
+        },
+        extensions = { "nvim-tree", "lazy" },
+      }
+    end,
+  },
+
   -- indent-blankline.nvim
   -- https://github.com/lukas-reineke/indent-blankline.nvim
   -- indent-guides for Neovim
@@ -62,5 +147,8 @@ return {
     end,
   },
 
-
+  -- nvim-web-devicons
+  -- https://github.com/nvim-tree/nvim-web-devicons
+  -- Fancy icon support (requires a Nerd font)
+  { "nvim-tree/nvim-web-devicons", lazy = true }
 }
