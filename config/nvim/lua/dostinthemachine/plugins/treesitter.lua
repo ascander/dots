29f06d42b2c79be1f99fc3299bf243c4d6ba2636
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-28 12:44:25 -0700

    Simplifies treesitter config

--- config/nvim/lua/dostinthemachine/plugins/treesitter.lua
+++ config/nvim/lua/dostinthemachine/plugins/treesitter.lua
@@ -1,26 +1,13 @@
 return {
   -- nvim-treesitter
   -- https://github.com/nvim-treesitter/nvim-treesitter
+  -- Syntax-aware highlighting, text objects, etc.
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+    version = false,
     build = ":TSUpdate",
     event = "VeryLazy",
     lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    keys = {
-      { "<c-space>", desc = "Increment Selection" },
-      { "<bs>", desc = "Decrement Selection", mode = "x" },
-    },
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
@@ -68,6 +55,7 @@ return {
 
   -- nvim-treesitter-textobjects
   -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/tree/master
+  -- Syntax-aware text objects
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     event = "VeryLazy",
@@ -99,8 +87,24 @@ return {
     end
   },
 
+  -- nvim-treesitter-context
+  -- https://github.com/nvim-treesitter/nvim-treesitter-context
+  -- Show code context
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    opts = {
+      mode = "cursor",
+      max_lines = 3,
+    },
+    keys = {
+      { "<leader>tt", function() require("treesitter-context").toggle() end, desc = "Toggle Treesitter Context" },
+    },
+  },
+
   -- nvim-ts-autotag
   -- https://github.com/windwp/nvim-ts-autotag
+  -- Auto-close and rename HTML tags, powered by Treesitter
   {
     "windwp/nvim-ts-autotag",
     event = "VeryLazy",
