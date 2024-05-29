d2d8660ba4bc8414c7c9ca0fef91bdf366874acc
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-28 17:06:08 -0700

    Adds nvim-spectre

--- config/nvim/lua/dostinthemachine/plugins/editor.lua
+++ config/nvim/lua/dostinthemachine/plugins/editor.lua
@@ -447,6 +447,20 @@ return {
     end,
   },
 
+  -- nvim-spectre
+  -- https://github.com/nvim-pack/nvim-spectre
+  -- Search/replace in multiple files
+  {
+    "nvim-pack/nvim-spectre",
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in Files (Spectre)" },
+    },
+  },
+
   -- persistence.nvim
   -- https://github.com/folke/persistence.nvim
   -- Save and restore Neovim sessions
