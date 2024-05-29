0755106084d0f08322dd25b5cc797419c299962e
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-29 14:24:43 -0700

    Adds nvim-metals

--- config/nvim/lua/dostinthemachine/plugins/lspconfig.lua
+++ config/nvim/lua/dostinthemachine/plugins/lspconfig.lua
@@ -272,6 +272,89 @@ return {
     end,
   },
 
+  -- nvim-metals
+  -- https://github.com/scalameta/nvim-metals
+  -- A Metals plugin for Neovim
+  {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "mfussenegger/nvim-dap",
+        config = function(self, opts)
+          -- Debug settings if you're using nvim-dap
+          local dap = require "dap"
+
+          dap.configurations.scala = {
+            {
+              type = "scala",
+              request = "launch",
+              name = "RunOrTest",
+              metals = {
+                runType = "runOrTestFile",
+              },
+            },
+            {
+              type = "scala",
+              request = "launch",
+              name = "Test Target",
+              metals = {
+                runType = "testTarget",
+              },
+            },
+          }
+        end,
+      },
+    },
+    ft = { "scala", "sbt", "java" },
+    keys = {
+      {
+        "<leader>cM",
+        function()
+          require("telescope").extensions.metals.commands()
+        end,
+        desc = "Metals commands",
+      },
+    },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+
+      metals_config.settings = {
+        fallbackScalaVersion = "2.12.15",
+        showImplicitArguments = true,
+        showImplicitConversionsAndClasses = true,
+        showInferredType = true,
+        excludedPackages = {
+          "akka.actor.typed.javadsl",
+          "com.github.swagger.akka.javadsl",
+          "akka.stream.javadsl",
+          "akka.http.javadsl",
+        },
+      }
+
+      metals_config.init_options.statusBarProvider = "on"
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      metals_config.on_attach = function()
+        require("metals").setup_dap()
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+
+      -- Start Metals when opening a Scala/SBT/Java file
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
+
   {
     "stevearc/aerial.nvim",
     cmd = "AerialToggle",
