7ff1615d4d5b71231fcecd8a9d8446885d0ec46a
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-05-30 10:54:00 -0700

    Fixes merge conflict resolution

--- flake.nix
+++ flake.nix
@@ -16,62 +16,31 @@
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    darwin,
-    home-manager,
-    neovim-nightly-overlay,
-    ...
-  } @ inputs: let
-    inherit (darwin.lib) darwinSystem;
-    inherit (inputs.nixpkgs.lib) attrValues optionalAttrs singleton;
+  outputs =
+    { self
+    , nixpkgs
+    , darwin
+    , home-manager
+    , neovim-nightly-overlay
+    , ...
+    } @ inputs:
+    let
+      inherit (darwin.lib) darwinSystem;
+      inherit (inputs.nixpkgs.lib) attrValues;
 
       currentSystem = "x86_64-darwin";
 
-    # Configuration for 'nixpkgs'
-    nixpkgsConfig = {
-      config = {allowUnfree = true;};
-      overlays = singleton neovim-nightly-overlay.overlay;
-    };
-  in {
-    overlays = {
-      # # Adds access to x86 packages through 'pkgs.x86' if running Apple Silicon
-      # apple-silicon = _: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-      #   x86 = import inputs.nixpkgs {
-      #     system = "x86_64-darwin";
-      #     inherit (nixpkgsConfig) config;
-      #   };
-      # };
-
-      # # Substitute x86 versions of packages that don't build on Apple Silicon yet
-      # sub-x86 = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-      #   inherit (final.x86);
-      # };
-    };
-
-    darwinModules = {
-      bootstrap = import ./darwin/bootstrap.nix;
-      defaults = import ./darwin/defaults.nix;
-      configuration = import ./darwin/configuration.nix;
-      homebrew = import ./darwin/homebrew.nix;
-    };
-
-    darwinConfigurations = {
-      adost-ltm = darwinSystem {
-        system = "x86_64-darwin";
-        modules =
-          attrValues self.darwinModules
-          ++ [
-            inputs.neovim-nightly-overlay.overlay
-          ];
+      # Configuration for 'nixpkgs'
+      nixpkgsConfig = {
+        config = { allowUnfree = true; };
+        overlays = [ neovim-nightly-overlay.overlay ];
       };
     in
     {
       overlays = {
-        # # Adds access to (unstable) x86 packages through 'pkgs.x86' if running Apple Silicon
+        # # Adds access to x86 packages through 'pkgs.x86' if running Apple Silicon
         # apple-silicon = _: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-        #   x86 = import inputs.nixpkgs-unstable {
+        #   x86 = import inputs.nixpkgs {
         #     system = "x86_64-darwin";
         #     inherit (nixpkgsConfig) config;
         #   };
@@ -81,31 +50,6 @@
         # sub-x86 = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
         #   inherit (final.x86);
         # };
-
-        # Adds access to unstable packages through 'pkgs.unstable'
-        pkgs-unstable = _: prev: {
-          unstable = import inputs.nixpkgs-unstable {
-            inherit (prev.stdenv) system;
-            inherit (nixpkgsConfig) config;
-          };
-        };
-
-        # Adds Neovim plugins not in `nixpkgs`
-        nvim-plugins = final: prev:
-          let
-            nvim-tree = prev.vimUtils.buildVimPlugin rec {
-              name = "nvim-tree";
-              src = inputs.nvim-tree;
-              version = src.lastModifiedDate;
-            };
-          in
-          {
-            vimPlugins =
-              prev.vimPlugins
-              // {
-                inherit nvim-tree;
-              };
-          };
       };
 
       darwinModules = {
