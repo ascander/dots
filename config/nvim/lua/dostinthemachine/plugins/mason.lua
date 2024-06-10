3ce502b73c0943b32d15f6582f85ec1d3c36fa50
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-06-10 14:01:19 -0700

    Removes type annotation

--- config/nvim/lua/dostinthemachine/plugins/mason.lua
+++ config/nvim/lua/dostinthemachine/plugins/mason.lua
@@ -13,7 +13,6 @@ return {
         "shfmt",
       },
     },
-    ---@param opts MasonSettings | {ensure_installed: string[]}
     config = function(_, opts)
       require("mason").setup(opts)
       local mr = require "mason-registry"
