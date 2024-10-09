ce0a9c64c197cc23e813a9975eeeac28d7055fc3
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-10-08 17:01:09 -0700

    Adds defaults for Dock/Trackpad

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -1,12 +1,41 @@
 {
+  # Dock settings
+  system.defaults.dock = {
+    autohide = true;
+    tilesize = 16;
+    mineffect = "scale";
+    minimize-to-application = true;
+    show-process-indicators = true;
+    showhidden = true;
+    show-recents = false;
+    persistent-apps = [ ];
+    persistent-others = [ ];
+    static-only = true;
+  };
+
+  # Trackpad settings
+  system.defaults.trackpad = {
+    ActuationStrength = 0;
+    Clicking = true;
+    Dragging = true;
+    TrackpadRightClick = true;
+    TrackpadThreeFingerDrag = true;
+    TrackpadThreeFingerTapGesture = 0;
+  };
+
+  # NSGlobalDomain settings (keyboard, trackpad, etc.)
   system.defaults.NSGlobalDomain = {
-    # Keyboard behavior
-    InitialKeyRepeat = 10;
+    # Set a fast key repeat rate
     KeyRepeat = 1;
+    InitialKeyRepeat = 10;
+
+    # Use function keys as standard function keys (F1, F2, etc.)
     "com.apple.keyboard.fnState" = true;
 
-    # Trackpad behavior
+    # Set a fast trackpad speed
     "com.apple.trackpad.scaling" = 3.0;
+
+    # Disable "natural" scrolling
     "com.apple.swipescrolldirection" = false;
 
     # Miscellaneous
@@ -24,4 +53,12 @@
     "com.apple.springing.delay" = 0.0;
     "com.apple.springing.enabled" = true;
   };
+
+  # Custom system settings
+  # TODO: try updating nix-darwin
+  # CustomSystemPreferences = {
+  #   NSGlobalDomain = {
+  #     AppleSpacesSwitchOnActivate = false;
+  #   };
+  # };
 }
