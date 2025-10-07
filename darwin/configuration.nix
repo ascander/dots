de71697e904dea4c1ad33344166870e31cce2ce2
Ascander Dost <1815984+ascander@users.noreply.github.com>
2025-10-07 12:04:50 -0700

    Adds iosevka NF variant

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }:
 {
   # Fonts
+  # https://nix-darwin.github.io/nix-darwin/manual/#opt-fonts.packages
   fonts.packages = with pkgs; [
     cooper-hewitt
     nerd-fonts.blex-mono
     nerd-fonts.fira-code
     nerd-fonts.fira-mono
+    nerd-fonts.iosevka
     source-sans
     source-serif
   ];
